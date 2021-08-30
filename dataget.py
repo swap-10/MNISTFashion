21f6661b67ba5d980db062b9b3f72411acb00d75
swap-10 <72344713+swap-10@users.noreply.github.com>
2021-08-30 13:53:20 +0530

    Add module to get data

---
+++ dataget.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+def dataget():
+    fashion_mnist = tf.keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    return train_images, train_labels, test_images, test_labels, class_names
+
+
+if __name__ == "__main__":
+    train_images, train_labels, test_images, test_labels, class_names = dataget()
